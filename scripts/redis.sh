f8e0ee1c73db3c734cee026cb3f2240d4cc961e9
jayone <imdff0803@gmail.com>
2020-05-31 21:31:19 +0900

    Add scripts for environment

---
+++ scripts/redis.sh
@@ -0,0 +1,5 @@
+docker run -d \
+    --rm \
+    --name redis \
+    -p 6379:6379 \
+    redis
---
+++ scripts/yolov3_download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+mkdir /yolo
+wget https://pjreddie.com/media/files/yolov3.weights -O /yolo/yolov3.weights
+wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg?raw=true -O /yolo/yolov3.cfg && \
+wget https://github.com/pjreddie/darknet/blob/master/data/coco.names?raw=true -O /yolo/coco.names
